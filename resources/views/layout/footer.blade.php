e87ace6c5c06d97069f06a6d62ed7af7e0e0edd8
KecoaxBunting <david.alvyn@binus.ac.id>
2023-12-19 14:47:31 +0700

    ALL DONE

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,4 +1,4 @@
 <link rel="stylesheet" href="{{ asset('css/layout/footer.css') }}">
-<div class="footer text-center">
+<div class="footer text-center fixed-bottom">
     <div class="h4 mb-0">TERNAKU @2023</div>
 </div>
